0fa5426a0ca41d27e7770b08e098214d4e52a696
Nihan Cj <nihancj.com@gmail.com>
2023-04-22 21:34:28 +0530

    feat: previewer

--- lua/telescope/_extensions/readme/main.lua
+++ lua/telescope/_extensions/readme/main.lua
@@ -5,12 +5,12 @@ local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 local themes = require("telescope.themes")
 
-local Scan = require "plenary.scandir"
-local path = vim.fn.stdpath('data') .. "/lazy"
-local readmes = Scan.scan_dir(path, {hidden=false,depth=2,only_dirs=false,search_pattern="README.md"})
+local Scan = require("plenary.scandir")
+local path = vim.fn.stdpath("data") .. "/lazy"
+local readmes = Scan.scan_dir(path, { hidden = false, depth = 2, only_dirs = false, search_pattern = "README.md" })
 
 local readme = function(opts)
-	opts = opts or themes.get_dropdown()
+	opts = themes["get_ivy"]()
 	pickers
 		.new(opts, {
 			prompt_title = "README",
@@ -22,14 +22,15 @@ local readme = function(opts)
 						display = string.gsub(vim.fs.dirname(entry), path .. "/", ""),
 						ordinal = entry,
 					}
-				end
+				end,
 			}),
+			previewer = conf.file_previewer(opts),
 			sorter = conf.generic_sorter(opts),
 			attach_mappings = function(prompt_bufnr, _)
 				actions.select_default:replace(function()
 					actions.close(prompt_bufnr)
 					local selection = action_state.get_selected_entry()
-					vim.cmd('view' .. selection["value"])
+					vim.cmd("view" .. selection["value"])
 				end)
 				return true
 			end,
